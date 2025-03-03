2b97ec5f403db74678f3c01e30c4f3b653e26f1b
Zheng He Hu <aeonzh@hotmail.com>
2025-03-03 01:28:50 +0100

    Clean up root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,11 @@
-import { useEffect } from 'react';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 
-declare global {
-  interface Window {
-    frameworkReady?: () => void;
-  }
-}
-
 export default function RootLayout() {
-  useEffect(() => {
-    window.frameworkReady?.();
-  }, []);
-
   return (
     <>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="index" />
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="shops/[id]" options={{ headerShown: false }} />
-        <Stack.Screen name="cart" />
-        <Stack.Screen name="orders/[id]" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
       <StatusBar style="auto" />
+      <Stack></Stack>
     </>
   );
 }

3691f980477e6d0efb925aa19c29bda9c0e1db02
Zheng He Hu <aeonzh@hotmail.com>
2025-06-29 16:33:54 +0200

    Fix expo babel configs

--- apps/buyer/babel.config.js
+++ apps/buyer/babel.config.js
@@ -1,21 +1,9 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      'expo-router/babel',
-      [
-        'nativewind/babel',
-        {
-          input: './global.css',
-          // Add shared package to the input for NativeWind processing
-          // This ensures that shared components' classNames are transformed
-          // correctly by NativeWind's Babel plugin.
-          // The path is relative to the monorepo root.
-          // This is a common pattern for monorepo setups with NativeWind.
-          allowModule: true, // Allow processing of modules, including shared package
-        },
-      ],
+    presets: [
+      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
+      'nativewind/babel',
     ],
   };
 };

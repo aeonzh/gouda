717245ef0f671103c30e857812b3628a4a295c74
Zheng He Hu <aeonzh@hotmail.com>
2025-06-29 20:20:52 +0200

    Setup supabase

---
+++ apps/admin/app.config.ts
@@ -0,0 +1,40 @@
+import { ExpoConfig, ConfigContext } from '@expo/config';
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: 'admin',
+  slug: 'admin',
+  version: '1.0.0',
+  orientation: 'portrait',
+  icon: './assets/icon.png',
+  userInterfaceStyle: 'light',
+  newArchEnabled: true,
+  splash: {
+    image: './assets/splash-icon.png',
+    resizeMode: 'contain',
+    backgroundColor: '#ffffff',
+  },
+  ios: {
+    supportsTablet: true,
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: './assets/adaptive-icon.png',
+      backgroundColor: '#ffffff',
+    },
+    edgeToEdgeEnabled: true,
+  },
+  web: {
+    bundler: 'metro',
+    favicon: './assets/favicon.png',
+  },
+  plugins: ['expo-router'],
+  experiments: {
+    typedRoutes: true,
+    tsconfigPaths: true,
+  },
+  extra: {
+    supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL ?? '',
+    supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY ?? '',
+  },
+});

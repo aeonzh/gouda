feb75b88279ea79b134432aba31f9204f0e6e367
Zheng He Hu <aeonzh@hotmail.com>
2025-06-29 13:49:10 +0200

    Use expo-router

--- apps/buyer/babel.config.js
+++ apps/buyer/babel.config.js
@@ -1,12 +1,13 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: ['babel-preset-expo'],
     plugins: [
+      'expo-router/babel',
       [
-        "nativewind/babel",
+        'nativewind/babel',
         {
-          input: "./global.css",
+          input: './global.css',
           // Add shared package to the input for NativeWind processing
           // This ensures that shared components' classNames are transformed
           // correctly by NativeWind's Babel plugin.

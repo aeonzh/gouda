ec79e26b1a9bbdf35720f2454d032f321e3f52d4
Zheng He Hu <aeonzh@hotmail.com>
2025-08-14 21:10:56 +0200

    build(b2b): add shared types and reorganize jest config

--- apps/b2b/jest-setup.js
+++ apps/b2b/jest-setup.js
@@ -2,8 +2,8 @@ import '@testing-library/jest-native/extend-expect';
 
 // Safe area
 jest.mock('react-native-safe-area-context', () => ({
-  useSafeAreaInsets: () => ({ top: 0, bottom: 0, left: 0, right: 0 }),
   SafeAreaProvider: ({ children }) => children,
+  useSafeAreaInsets: () => ({ bottom: 0, left: 0, right: 0, top: 0 }),
 }));
 
 // expo-constants
--- apps/b2b/jest.config.js
+++ apps/b2b/jest.config.js
@@ -1,14 +1,14 @@
 /** @type {import('jest').Config} */
 module.exports = {
-  preset: 'jest-expo',
-  setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
-  transformIgnorePatterns: [
-    'node_modules/(?!(expo|@expo|react-native|@react-native|expo-router|@react-navigation|@supabase/supabase-js|@react-native-picker/picker|expo-modules-core)/)',
-  ],
   moduleNameMapper: {
     '^@react-native/js-polyfills/error-guard$':
       '<rootDir>/../../__mocks__/@react-native/js-polyfills/error-guard.js',
     'shared/(.*)': '<rootDir>/../../packages/shared/$1',
   },
+  preset: 'jest-expo',
+  setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
   testEnvironment: 'jsdom',
+  transformIgnorePatterns: [
+    'node_modules/(?!(expo|@expo|react-native|@react-native|expo-router|@react-navigation|@supabase/supabase-js|@react-native-picker/picker|expo-modules-core)/)',
+  ],
 };

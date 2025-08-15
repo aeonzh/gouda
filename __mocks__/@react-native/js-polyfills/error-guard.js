2002c6026277b53d3907f18cdf77e6555710ca59
Zheng He Hu <aeonzh@hotmail.com>
2025-08-15 10:45:12 +0200

    chore(test): remove jest mock and global type declaration files

--- __mocks__/@react-native/js-polyfills/error-guard.js
+++
@@ -1,6 +0,0 @@
-// Jest mock for React Native's error-guard polyfill used in the RN test setup
-module.exports = {
-  __esModule: true,
-  default: function noop() {},
-  reportFatalError: function noop() {},
-};
